4e8fd56cefd26acaba41eb82516b16496a3f6a4f
yuta asakura <yuta.asakura.se@gmail.com>
2026-01-15 22:34:54 +0900

    Add axios dependency and update .gitignore to include .env file; refactor App component to use absolute imports for NavBar and add GameGrid component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Grid, GridItem } from "@chakra-ui/react";
-import NavBar from "./components/NavBar";
+import NavBar from "@/components/NavBar";
+import GameGrid from "@/components/GameGrid";
 function App() {
   return (
     <Grid
@@ -11,11 +12,11 @@ function App() {
       <GridItem area="nav">
         <NavBar />
       </GridItem>
-      <GridItem area="aside" bg="gold" display={{ base: "none", lg: "block" }}>
+      <GridItem area="aside" display={{ base: "none", lg: "block" }}>
         Aside
       </GridItem>
-      <GridItem area="main" bg="dodgerblue">
-        Main
+      <GridItem area="main">
+        <GameGrid />
       </GridItem>
     </Grid>
   );
---
+++ src/services/api-client.ts
@@ -0,0 +1,9 @@
+import axios from "axios";
+const apiKey = import.meta.env.VITE_RAWG_API_KEY
+
+export default axios.create({
+  baseURL: "https://api.rawg.io/api",
+  params: {
+    key: apiKey,
+  }
+});
