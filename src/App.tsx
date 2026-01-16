a0b457cc3be76e934f61e8a8dc737dc7ccd051e2
yuta asakura <yuta.asakura.se@gmail.com>
2026-01-16 17:47:40 +0900

    Add PlatformSelector component to App for platform selection; implement usePlatforms hook to fetch platform data and display in a dropdown menu.

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import GameGrid from "@/components/GameGrid";
 import GenreList from "@/components/GenreList";
 import { useState } from "react";
 import type { Genre } from "./hooks/useGenres";
+import PlatformSelector from "./components/PlatformSelector";
 function App() {
   const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null);
   return (
@@ -24,6 +25,7 @@ function App() {
         <GenreList onSelectGenre={(genre) => setSelectedGenre(genre)} selectedGenre={selectedGenre} />
       </GridItem>
       <GridItem area="main">
+        <PlatformSelector />
         <GameGrid selectedGenre={selectedGenre} />
       </GridItem>
     </Grid>
---
+++ src/components/PlatformSelector.tsx
@@ -0,0 +1,29 @@
+import usePlatforms from "@/hooks/usePlatforms";
+import { Button, Menu, Portal } from "@chakra-ui/react";
+import { BsChevronDown } from "react-icons/bs";
+
+const PlatformSelector = () => {
+  const { data, error } = usePlatforms();
+  if (error) return null;
+  return (
+    <Menu.Root>
+      <Menu.Trigger>
+        <Button variant="subtle">
+          Platform
+          <BsChevronDown />
+        </Button>
+      </Menu.Trigger>
+      <Portal>
+        <Menu.Positioner>
+          <Menu.Content>
+            {data.map((platform) => (
+              <Menu.Item key={platform.id} value={platform.slug}>{platform.name}</Menu.Item>
+            ))}
+          </Menu.Content>
+        </Menu.Positioner>
+      </Portal>
+    </Menu.Root>
+  );
+};
+
+export default PlatformSelector;
