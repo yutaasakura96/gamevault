a0eb6e1ed5b509521005336ffa0049eddb79a6a4
yuta asakura <yuta.asakura.se@gmail.com>
2026-01-16 23:55:48 +0900

    Enhance sorting functionality in App by integrating sortOrder state; update SortSelector to display current sort option and handle sort order changes. Modify useGames hook to include sortOrder in data fetching parameters.

--- src/components/SortSelector.tsx
+++ src/components/SortSelector.tsx
@@ -1,24 +1,42 @@
-import { Button, Menu, MenuItem, Portal } from "@chakra-ui/react";
+import { Button, Menu, Portal } from "@chakra-ui/react";
 import { BsChevronDown } from "react-icons/bs";
 
-const SortSelector = () => {
+interface Props {
+  onSelectSortOrder: (sortOrder: string) => void;
+  sortOrder: string;
+}
+
+const SortSelector = ({ onSelectSortOrder, sortOrder }: Props) => {
+  const sortOrders = [
+    { value: "", label: "Relevance" },
+    { value: "added", label: "Date Added" },
+    { value: "name", label: "Name" },
+    { value: "-released", label: "Release Date" },
+    { value: "-metacritic", label: "Popularity" },
+    { value: "-rating", label: "Average Rating" },
+  ];
+
+  const currentSortOrder = sortOrders.find((order) => order.value === sortOrder);
   return (
     <Menu.Root>
       <Menu.Trigger asChild>
         <Button variant="subtle">
-          order by: Relevance
+          order by: {currentSortOrder?.label || "Relevance"}
           <BsChevronDown />
         </Button>
       </Menu.Trigger>
       <Portal>
         <Menu.Positioner>
           <Menu.Content>
-            <Menu.Item value="Relevance">Relevance</Menu.Item>
-            <Menu.Item value="Date added">Date added</Menu.Item>
-            <Menu.Item value="Name">Name</Menu.Item>
-            <Menu.Item value="Release date">Release date</Menu.Item>
-            <Menu.Item value="Popularity">Popularity</Menu.Item>
-            <MenuItem value="Average rating">Average rating</MenuItem>
+            {sortOrders.map((order) => (
+              <Menu.Item
+                onClick={() => onSelectSortOrder(order.value)}
+                key={order.value}
+                value={order.value}
+              >
+                {order.label}
+              </Menu.Item>
+            ))}
           </Menu.Content>
         </Menu.Positioner>
       </Portal>
