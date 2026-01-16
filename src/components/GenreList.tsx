87a62befa687d58c9560280101c2853062bb2e5a
yuta asakura <yuta.asakura.se@gmail.com>
2026-01-16 15:11:18 +0900

    Enhance GenreList component by adding loading and error handling states; display a spinner while fetching genres to improve user experience.

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,9 +1,11 @@
 import useGenres from "@/hooks/useGenres";
 import getCroppedImageUrl from "@/services/image-url";
-import { HStack, Image, List, Text } from "@chakra-ui/react";
+import { HStack, Image, List, Spinner, Text } from "@chakra-ui/react";
 
 const GenreList = () => {
-  const { data } = useGenres();
+  const { data, isLoading, error } = useGenres();
+  if (error) return null;
+  if (isLoading) return <Spinner />;
   return (
     <List.Root listStyleType="none">
       {data.map((genre) => (
