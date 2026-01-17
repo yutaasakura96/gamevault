49e90398c2f7e1a08c8c4d8d35e6b1d5b7bb2ff1
yuta asakura <yuta.asakura.se@gmail.com>
2026-01-17 16:38:09 +0900

    Refactor GameCard and GameGrid components for improved layout and responsiveness; adjust GameCard image box to use aspect ratio and modify GameGrid to reduce column count, enhancing overall UI presentation.

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -10,7 +10,7 @@ interface Props {
 const GameCard = ({ game }: Props) => {
   return (
     <Card.Root>
-      <Box height="200px">
+      <Box aspectRatio={3/2} overflow="hidden">
         <Image
           src={getCroppedImageUrl(game.background_image)}
           alt={game.name}
@@ -21,11 +21,11 @@ const GameCard = ({ game }: Props) => {
         />
       </Box>
       <Card.Body gap="2">
-        <Card.Title fontSize={"xl"}>{game.name}</Card.Title>
-        <HStack justifyContent="space-between">
+        <HStack justifyContent="space-between" marginBottom={3}>
           <PlatformIconList platforms={game.parent_platforms.map((p) => p.platform)} />
           <CriticScore score={game.metacritic} />
         </HStack>
+        <Card.Title fontSize={"xl"}>{game.name}</Card.Title>
       </Card.Body>
     </Card.Root>
   );
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -15,7 +15,7 @@ const GameGrid = ({ gameQuery }: Props) => {
   return (
     <>
       {error && <Text>{error}</Text>}
-      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 5 }} padding="10px" gap={3}>
+      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} padding="10px" gap={6}>
         {isLoading &&
           skeletons.map((skeleton) => (
             <GameCardContainer key={skeleton}>
