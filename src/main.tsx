2416c85611e391185b8741741172585f521b1ff3
yuta asakura <yuta.asakura.se@gmail.com>
2026-01-19 18:41:39 +0900

    Add React Query and Devtools for improved data fetching and state management; update package dependencies accordingly.

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,18 @@ import { createRoot } from 'react-dom/client'
 import { Provider } from './components/ui/provider.tsx'
 import './index.css'
 import App from './App.tsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </Provider>
   </StrictMode>,
 )
