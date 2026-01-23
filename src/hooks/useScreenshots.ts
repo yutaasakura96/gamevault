bc2f10c043a548fce94f7e775c6b5f404d38e034
yuta asakura <yuta.asakura.se@gmail.com>
2026-01-23 23:27:46 +0900

    Add GameScreenshots component and useScreenshots hook for fetching and displaying game screenshots; introduce Screenshot interface for type management.

---
+++ src/hooks/useScreenshots.ts
@@ -0,0 +1,13 @@
+import type { Screenshot } from "@/entities/Screenshot";
+import APIClient from "@/services/api-client";
+import { useQuery } from "@tanstack/react-query";
+
+const useScreenshots = (gameId: number) => {
+  const apiClient = new APIClient<Screenshot>(`/games/${gameId}/screenshots`);
+  return useQuery({
+    queryKey: ['screenshots', gameId],
+    queryFn: apiClient.getAll,
+  });
+};
+
+export default useScreenshots;
