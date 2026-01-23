b791cbe47d3dad352ebba58d2c8f5f7b01aff51d
yuta asakura <yuta.asakura.se@gmail.com>
2026-01-23 11:26:07 +0900

    Update package dependencies to include react-router-dom for routing; remove App component and associated styles, replacing it with RouterProvider for improved routing management.

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from './components/ui/provider.tsx'
 import './index.css'
-import App from './App.tsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { RouterProvider } from 'react-router-dom'
+import router from './routes.tsx'
 
 const queryClient = new QueryClient()
 
@@ -12,7 +13,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider>
       <QueryClientProvider client={queryClient}>
-        <App />
+        <RouterProvider router={router} />
         <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
     </Provider>
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,26 +1,22 @@
-import { Box, Grid, GridItem, HStack } from "@chakra-ui/react";
-import NavBar from "@/components/NavBar";
-import GameGrid from "@/components/GameGrid";
-import GenreList from "@/components/GenreList";
-import PlatformSelector from "./components/PlatformSelector";
-import SortSelector from "./components/SortSelector";
-import GameHeading from "./components/GameHeading";
+import GameGrid from '@/components/GameGrid'
+import GameHeading from '@/components/GameHeading'
+import GenreList from '@/components/GenreList'
+import PlatformSelector from '@/components/PlatformSelector'
+import SortSelector from '@/components/SortSelector'
+import { Box, Grid, GridItem, HStack } from '@chakra-ui/react'
 
-function App() {
+const HomePage = () => {
   return (
     <Grid
       templateAreas={{
-        base: `"nav" "main"`,
-        lg: `"nav nav" "aside main"`,
+        base: `"main"`,
+        lg: `"aside main"`,
       }}
       templateColumns={{
         base: "1fr",
         lg: "200px 1fr",
       }}
     >
-      <GridItem area="nav">
-        <NavBar />
-      </GridItem>
       <GridItem area="aside" paddingX={5} display={{ base: "none", lg: "block" }}>
         <GenreList />
       </GridItem>
@@ -35,7 +31,7 @@ function App() {
         <GameGrid />
       </GridItem>
     </Grid>
-  );
+  )
 }
 
-export default App;
+export default HomePage
