f43a5a1facbf406b00a1d8c3c67c85ec4e45d49a
yuta asakura <yuta.asakura.se@gmail.com>
2026-01-24 00:00:12 +0900

    Enhance NavBar and SearchInput components with routing functionality; wrap logo in a Link for navigation and update search submission to navigate to the home page after setting search text.

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,13 +1,15 @@
 import { HStack, Image, Text } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 import logo from "@/assets/logo.webp";
 import { ColorModeSwitch } from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
 
-
 const NavBar = () => {
   return (
     <HStack padding="10px">
-      <Image src={logo} boxSize="60px" />
+      <Link to="/" >
+        <Image src={logo} boxSize="60px" cursor="pointer" objectFit="cover" />
+      </Link>
       <Text fontSize="2xl" fontWeight="bold">GameVault</Text>
       <SearchInput />
       <ColorModeSwitch />
--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -2,14 +2,19 @@ import { Input, InputGroup } from "@chakra-ui/react";
 import { useRef } from "react";
 import { BsSearch } from "react-icons/bs";
 import useGameQueryStore from "@/store";
+import { useNavigate } from "react-router-dom";
 
 const SearchInput = () => {
   const ref = useRef<HTMLInputElement>(null);
   const setSearchText = useGameQueryStore((state) => state.setSearchText);
+  const navigate = useNavigate();
   return (
     <form onSubmit={(event) => {
       event.preventDefault()
-      if (ref.current) setSearchText(ref.current.value);
+      if (ref.current) {
+        setSearchText(ref.current.value);
+        navigate(`/`);
+      }
     }}>
       <InputGroup startElement={<BsSearch />}>
         <Input ref={ref} borderRadius="20px" placeholder="Search games..." variant="subtle" />
